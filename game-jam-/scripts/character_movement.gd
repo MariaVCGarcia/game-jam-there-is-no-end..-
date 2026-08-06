b09ad05e97c9fc5a29abf5714134d1b23c045e4b
MariaVCGarcia <120209017+MariaVCGarcia@users.noreply.github.com>
2026-08-06 13:42:24 -0400

    update
    redid the level by adding the tile map and added a prper bean sprite. added double jump

--- game-jam-/scripts/character_movement.gd
+++ game-jam-/scripts/character_movement.gd
@@ -1,4 +1,5 @@
 extends CharacterBody2D
+
 const SPEED = 800.0
 const JUMP_VELOCITY = -800.0
 func _physics_process(delta: float) -> void:
@@ -11,6 +12,10 @@ func _physics_process(delta: float) -> void:
 	#releases the jump allowing gravity to kick in
 	if Input.is_action_just_released("up ") and not is_on_floor():
 		velocity.y -= JUMP_VELOCITY *.3
+	#dubble jump 
+	if Input.is_action_just_pressed("up ") and not is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	#fast fall 
 	if Input.is_action_pressed("down") and not is_on_floor():
 		velocity.y -= JUMP_VELOCITY *.1
 	# Get the input direction and handle the movement/deceleration.
