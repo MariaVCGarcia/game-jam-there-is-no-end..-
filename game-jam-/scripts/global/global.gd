51fd6a904bef724b2aea5519bb2c9cc64e7548d8
MariaVCGarcia <120209017+MariaVCGarcia@users.noreply.github.com>
2026-08-10 12:00:35 -0400

    end screen has been conneted

--- game-jam-/scripts/global/global.gd
+++ game-jam-/scripts/global/global.gd
@@ -3,6 +3,7 @@ extends Node
 const scene_level = preload("res://scenes/level_1.tscn")
 const scene_level2 = preload("res://scenes/level_2.tscn")
 const scene_level3 = preload("res://scenes/level_3.tscn")
+const scene_end = preload("res://scenes/end_scene.tscn")
 var spwan_door_tag 
 # level transition contorler 
 func level_transitions(level_tag, destination_tag):
@@ -14,6 +15,8 @@ func level_transitions(level_tag, destination_tag):
 			scene_to_load = scene_level2
 		"level_3":
 			scene_to_load = scene_level3
+		"end_scene":
+			scene_to_load = scene_end
 	if scene_to_load !=null:
 		spwan_door_tag = destination_tag
 		get_tree().change_scene_to_packed(scene_to_load)
